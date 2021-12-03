6bea2b2c00d7e354e2954e2d5bf91c53a6a4f2d4
juniorflavio_oficial <59364674+Jobsfotogrid@users.noreply.github.com>
2021-12-03 20:18:46 -0300

    Arquitetura de Software: DataSources

---
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/data/ListCategoryCallback.kt
@@ -0,0 +1,11 @@
+package co.tiagoaguiar.tutorial.jokerappdev.data
+
+interface ListCategoryCallback {
+
+    fun onSucess(response: List<String>)
+
+    fun onError(response: String)
+
+    fun onComplete()
+
+}

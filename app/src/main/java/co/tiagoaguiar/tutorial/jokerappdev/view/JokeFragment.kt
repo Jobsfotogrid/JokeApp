40aed7eca7b59b12d787ffe97ffcb7724ac2836f
juniorflavio_oficial <59364674+Jobsfotogrid@users.noreply.github.com>
2021-12-05 08:17:22 -0300

    Biblioteca Moderna 4: Download de Imagens e Cache

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/view/JokeFragment.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/view/JokeFragment.kt
@@ -14,6 +14,7 @@ import co.tiagoaguiar.tutorial.jokerappdev.R
 import co.tiagoaguiar.tutorial.jokerappdev.model.Joke
 import co.tiagoaguiar.tutorial.jokerappdev.presentation.JokePresenter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.squareup.picasso.Picasso
 
 class JokeFragment : Fragment() {
 
@@ -33,7 +34,11 @@ class JokeFragment : Fragment() {
         presenter = JokePresenter(this)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_joke, container, false)
     }
 
@@ -44,6 +49,7 @@ class JokeFragment : Fragment() {
         activity?.findViewById<Toolbar>(R.id.toolbar)?.title = categoryName
         progressBar = view.findViewById(R.id.progress_bar)
         textView = view.findViewById(R.id.txt_joke)
+        imageView = view.findViewById(R.id.img_joke)
 
         view.findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
             presenter.findBy(categoryName)
@@ -54,7 +60,7 @@ class JokeFragment : Fragment() {
 
     fun showJoke(joke: Joke) {
         textView.text = joke.text
-        // TODO: adicionar imagem
+        Picasso.get().load(joke.iconUrl).into(imageView)
     }
 
     fun showProgress() {
